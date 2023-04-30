03e9fdf42067968af6eaae9f1959e6d58f9cf20b
vivianna ramirez <vivrami2001@gmail.com>
2023-04-29 17:11:47 -0700

    complete lab

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words=tutorial.split(' ')
+    let capitalizedWords= words.map(word =>
+      word.charAt(0).toUpperCase()+ word.slice(1))
+      let result = capitalizedWords.join(' ')
+      return result
+  })
 }
